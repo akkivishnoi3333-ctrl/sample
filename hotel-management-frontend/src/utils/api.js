cc9a987227541ef8c03807c2d6253ded3c7a15b3
akkivishnoi3333-ctrl <akkivishnoi3333@gmail.com>
2026-04-21 22:17:16 +0530

    Update api.js

--- hotel-management-frontend/src/utils/api.js
+++ hotel-management-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:"https://sample-3-pf1p.onrender.com/api"
+  baseURL:"https://sample-4-pf1p.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
